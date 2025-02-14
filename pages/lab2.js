566153bd9e28d3b7fdee7566b4b3654c7a4687cb
Gana123-droid <caatkety@gmail.com>
2025-02-14 14:43:42 +0800

    monoglapi

---
+++ pages/lab2.js
@@ -0,0 +1,36 @@
+import {useEffect, useState} from "react";   
+export default function Lab2(){
+
+    const [data, setData] = useState("");
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const response = await fetch(
+                    "https://mongol-api-rest.vercel.app/touristAttractions"
+                );
+                const result = await response.json();
+                setData(result);
+            } catch (error){
+                console.log(error);
+            }
+        };
+        fetchData();
+    }, []);
+    console.log(data);
+    return(
+        <div className="grid grid-cols-4 gap-4 bg-white max-h-full w-full ">
+            {data?.touristAttractions?.map((item) => (
+                <div className="text-black border border-black shadow-lg rounded-md w-[400px] ">
+                  <img
+              src={item.images[0]} 
+              alt={item.name}
+              className="w-full h-56 object-cover border rounded-lg mb-4 "
+            />
+                    <p  className="text-xl font-bold text-center">{item?.name}</p> 
+                   <p className="text-center">{item?.description}</p>  
+                  <p className="opacity-75 text-center">{item?.address?.country}</p>                      
+                </div>
+            ))}
+        </div>
+    )
+}
