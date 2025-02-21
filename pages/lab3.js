3b0eb48e4be739159a19d282ffca36263086d9a2
Gana123-droid <caatkety@gmail.com>
2025-02-21 14:54:28 +0800

    lab3

--- pages/lab3.js
+++ pages/lab3.js
@@ -3,11 +3,6 @@ import Image from "next/image";
 import { useRouter } from 'next/router';
 
 export default function Lab3() {
-    const [search, setSearch] = useState("");
-    const filteredData = data.filter((items) =>
-        items.name.toLowerCase().includes(search.toLowerCase())
-      );
-      const router = useRouter();
   const [data, setData] = useState("");
 
   useEffect(() => {
