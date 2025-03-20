8278d17112655ec21e2e067ba09c7a88922e3b90
ganaa <caatkety@gmail.com>
2025-03-20 22:20:26 +0800

    lab3

--- pages/11b/index.js
+++ pages/11b/index.js
@@ -68,61 +68,71 @@ export default function Sfilter() {
 
   return (
     <div className="w-full min-h-screen bg-white">
-      {loading && <p className="text-blue-400 text-lg text-center">...Loading</p>}
-      
-      
-        <div className="w-full flex items-center flex-col gap-3 relative">
-          <div className="flex justify-between mt-4 mx-8 fixed absolute top-1 right-1">
-            <button
-              className="lg:text-2xl sm:text-lg md:text-xl text-blue-500 underline font-bold border border-black sm:w-[50px] sm:h[25px] md:w-[80px] lg:h-[40px]  lg:w-[100px] lg:h-[50px] rounded-lg"
-              onClick={() => setGrid(!grid)}
+      {loading ? (
+        <div className="w-full h-screen flex justify-center items-center flex-col">
+        <div className="w-32 h-32 rounded-full border-4 border-blue-500 border-t-transparent bg-transparent animate-spin">
+          
+        </div>
+        <div className="flex flex-row"><div className="text-xl text-black">Loading</div>
+        <div className="animate-bounce text-xl  text-black">.</div>
+        <div className="animate-bounce text-xl    text-black" style={{ animationDelay: "0.2s" }}>.</div>
+        <div className="animate-bounce text-xl   text-black" style={{ animationDelay: "0.4s" }}>.</div>
+        </div>
+        </div>
+      ) : (
+        <>
+          <div className="w-full flex items-center flex-col gap-3">
+            <div className="flex justify-between mt-4 mx-8 fixed top-1 right-1">
+              <button
+                className="lg:text-2xl sm:text-lg md:text-xl text-blue-500 underline font-bold border border-black sm:w-[50px] sm:h[25px] md:w-[80px] lg:w-[100px] lg:h-[50px] rounded-lg"
+                onClick={() => setGrid(!grid)}
+              >
+                {grid ? "Ungrid" : "Grid"}
+              </button>
+            </div>
+            <p className="font-bold text-xl text-black">Mongol-api</p>
+            <input
+              className="text-black border border-black w-1/2 p-2 rounded shadow-lg"
+              type="search"
+              placeholder="Search..."
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+
+          <div className="flex justify-center">
+            <div
+              className={`grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10 ${
+                grid ? "grid md:grid-cols-1 lg:grid-cols-1 w-[650px]" : ""
+              }`}
             >
-              {grid ? "Ungrid" : "Grid"}
-            </button>
-          </div>
-          <p className="font-bold text-xl text-black">Mongol-api</p>
-          <input
-            className="text-black border border-black w-1/2 p-2 rounded shadow-lg"
-            type="search"
-            onChange={(e) => setSearch(e.target.value)}
-          />
-        </div>
-      
-      
-        <div className="flex justify-center">
-          <div
-            className={`grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10 ${
-              grid ? "grid md:grid-cols-1 lg:grid-cols-1 w-[650px]" : ""
-            }`}
-          >
-            {filteredData.length > 0 ? (
-              filteredData.map((item) => (
-                <div
-                  key={item.name}
-                  className="text-black border border-black shadow-lg rounded-md p-4" onClick={() => router.push(`/11b/${item.id}`)}
-                >
-                  <img
-                    src={item.images}
-                    alt={item.name}
-                    className="w-full h-56 object-cover rounded-lg mb-4"
-                  />
-                  <p className="text-xl font-bold text-center">{item.name}</p>
-                  <p className="text-center">{item.description}</p>
-                  {item.address?.country && (
-                    <p className="opacity-75 text-center">
-                      {item.address.country}
-                    </p>
-                  )}
+              {filteredData.length > 0 ? (
+                filteredData.map((item) => (
+                  <div
+                    key={item.id}
+                    className="text-black border border-black shadow-lg rounded-md p-4 cursor-pointer"
+                    onClick={() => router.push(`/11b/${item.id}`)}
+                  >
+                    <img
+                      src={item.images}
+                      alt={item.name}
+                      className="w-full h-56 object-cover rounded-lg mb-4"
+                    />
+                    <p className="text-xl font-bold text-center">{item.name}</p>
+                    <p className="text-center">{item.description}</p>
+                    {item.address?.country && (
+                      <p className="opacity-75 text-center">{item.address.country}</p>
+                    )}
+                  </div>
+                ))
+              ) : (
+                <div className="text-black text-lg col-span-4 text-center">
+                  No results found for: <b>{search}</b>
                 </div>
-              ))
-            ) : (
-              <div className="text-black text-lg col-span-4 text-center">
-                No results found for: <b>{search}</b>
-              </div>
-            )}
+              )}
+            </div>
           </div>
-        </div>
-      
+        </>
+      )}
     </div>
   );
 }
